a1c23213182071555bb55d9ce9bb20cce17c4e54
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-04-19 19:45:09 +0530

    3rd EP notes

---
+++ code/EP-01/app.js
@@ -0,0 +1,8 @@
+let name = 'Satyam Bhardwaj';
+let age = 20;
+
+let number = 10;
+let number2 = 20;
+
+console.log(`Hello ${name}, you are ${age} years old.`);
+console.log(`The sum of ${number} and ${number2} is ${number + number2}.`);
