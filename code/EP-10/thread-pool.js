e4a1259330c7ca45f0824958634bf7f7cfba3ea3
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2025-04-26 13:02:25 +0530

    S-01-EP-10 Code about Thread Pool

---
+++ code/EP-10/thread-pool.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+process.env.UV_THREADPOOL_SIZE = 6;
+
+crypto.pbkdf2('password', 'salt', 5000000, 50, 'sha512', (err, key) => {
+    console.log("1 - crypto.pbkdf2 done");
+    
+});
+crypto.pbkdf2('password', 'salt', 5000000, 50, 'sha512', (err, key) => {
+    console.log("2 - crypto.pbkdf2 done");
+    
+});
+crypto.pbkdf2('password', 'salt', 5000000, 50, 'sha512', (err, key) => {
+    console.log("3 - crypto.pbkdf2 done");
+    
+});
+
+
+crypto.pbkdf2('password', 'salt', 5000000, 50, 'sha512', (err, key) => {
+    console.log("4 - crypto.pbkdf2 done");
+    
+});
+crypto.pbkdf2('password', 'salt', 5000000, 50, 'sha512', (err, key) => {
+    console.log("5 - crypto.pbkdf2 done");
+    
+});
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+    console.log('file read done');
+});
